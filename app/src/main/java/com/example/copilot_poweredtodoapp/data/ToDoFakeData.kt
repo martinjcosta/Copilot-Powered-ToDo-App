b776d462a63c07e38ce41a2a3dc38af0c484af1f
Martin Costa <martin.costa@fanduel.com>
2022-09-29 19:04:48 -0400

    ToDo items

---
+++ app/src/main/java/com/example/copilot_poweredtodoapp/data/ToDoFakeData.kt
@@ -0,0 +1,10 @@
+package com.example.copilot_poweredtodoapp.data
+
+val ToDoFakeData = listOf(
+    ToDoItem("Buy milk", "2% milk", false),
+    ToDoItem("Buy eggs", "12 eggs", false),
+    ToDoItem("Buy bread", "Whole wheat bread", false),
+    ToDoItem("Buy cheese", "Cheddar cheese", false),
+    ToDoItem("Buy butter", "Salted butter", false),
+    ToDoItem("Buy yogurt", "Greek yogurt", false),
+)
---
+++ app/src/main/java/com/example/copilot_poweredtodoapp/data/ToDoItem.kt
@@ -0,0 +1,7 @@
+package com.example.copilot_poweredtodoapp.data
+
+data class ToDoItem(
+    val title: String,
+    val description: String,
+    val isDone: Boolean
+)

70a0afdfc57c81407de11a108c68e4c66113e9f6
Martin Costa <martin.costa@fanduel.com>
2022-09-29 19:20:23 -0400

    Todo list UI

---
+++ app/src/main/java/com/example/copilot_poweredtodoapp/ui/ToDoList.kt
@@ -0,0 +1,72 @@
+package com.example.copilot_poweredtodoapp.ui
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.Checkbox
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.copilot_poweredtodoapp.data.ToDoFakeData
+import com.example.copilot_poweredtodoapp.data.ToDoItem
+
+
+// My To-do Item Compose UI with a checkbox, title, and description
+@Composable
+fun ToDoItem(
+    toDoItem: ToDoItem,
+    onCheckedChange: (Boolean) -> Unit
+) {
+    Row(
+        modifier = Modifier.fillMaxWidth()
+    ) {
+        Checkbox(
+            checked = toDoItem.isDone,
+            onCheckedChange = onCheckedChange
+        )
+        Column {
+            Text(text = toDoItem.title)
+            Text(text = toDoItem.description)
+        }
+    }
+}
+
+// To-Do List that takes a list of ToDoItem and places them in a LazyColumn
+@Composable
+fun ToDoList(
+    toDoList: List<ToDoItem>,
+    onCheckedChange: (Int, Boolean) -> Unit
+) {
+    LazyColumn {
+        items(items = toDoList, itemContent = { toDoItem ->
+            ToDoItem(
+                toDoItem = toDoItem,
+                onCheckedChange = { isChecked ->
+                    onCheckedChange(toDoList.indexOf(toDoItem), isChecked)
+                }
+            )
+        })
+    }
+}
+
+
+@Preview
+@Composable
+fun ToDoListPreview() {
+    ToDoList(
+        toDoList = ToDoFakeData,
+        onCheckedChange = { _, _ -> }
+    )
+}
+
+@Preview
+@Composable
+fun ToDoItemPreview() {
+    ToDoItem(
+        toDoItem = ToDoFakeData[0],
+        onCheckedChange = { }
+    )
+}

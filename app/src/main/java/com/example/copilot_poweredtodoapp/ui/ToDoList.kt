5cf1258e9edd768f77cebd1c032f90142f6ab166
Martin Costa <martin.costa@fanduel.com>
2022-09-29 21:39:08 -0400

    Add drag handle

--- app/src/main/java/com/example/copilot_poweredtodoapp/ui/ToDoList.kt
+++ app/src/main/java/com/example/copilot_poweredtodoapp/ui/ToDoList.kt
@@ -1,5 +1,6 @@
 package com.example.copilot_poweredtodoapp.ui
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
@@ -9,8 +10,10 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.example.copilot_poweredtodoapp.R
 import com.example.copilot_poweredtodoapp.data.ToDoFakeData
 import com.example.copilot_poweredtodoapp.data.ToDoItem
 
@@ -50,6 +53,19 @@ fun ToDoItem(
                     modifier = Modifier.padding(start = 8.dp)
                 )
             }
+            Spacer(modifier = Modifier.weight(1f))
+
+
+            // Handle that allows the user to drag and drop the item
+            // At the end of the ToDoItem
+            Image(
+                modifier = Modifier
+                    .align(Alignment.CenterVertically)
+                    .padding(start = 8.dp)
+                    .size(24.dp),
+                painter = painterResource(id = R.drawable.ic_drag_handle),
+                contentDescription = "Drag Handle"
+            )
         }
     }
 }
